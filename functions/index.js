fd7938063c08e426b300f997344f6f6ba826dd17
omarchaar <omar@nvrl8.com>
2022-06-24 19:36:45 -0300

    added addUser

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,9 @@
 const functions = require("firebase-functions");
-
 const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-/*
-    FETCH ALL DEALERSHIPS
-*/
+// FETCH ALL DEALERSHIPS
 exports.dealerships = functions.https.onRequest(async (req, res) => {
     const snapshot = await admin.firestore().collection("dealerships").get();
     
@@ -25,18 +22,14 @@ exports.dealerships = functions.https.onRequest(async (req, res) => {
     res.json(dealerships);
 });
 
-/*
-    FETCH SPECIFIC DEALERSHIP ACCORDING TO UNIQUE ID
-*/
+// FETCH SPECIFIC DEALERSHIP ACCORDING TO UNIQUE ID
 exports.dealership = functions.https.onRequest(async (req, res) => {
     const snapshot = await admin.firestore().collection("dealerships").doc(req.body.id).get();
 
     res.json(snapshot.data());
 });
 
-/*
-    FETCH ALL VEHICLES
-*/
+// FETCH ALL VEHICLES
 exports.vehicles = functions.https.onRequest(async (req, res) => {
     const snapshot = await admin.firestore().collection("vehicles").get();
     
@@ -55,11 +48,29 @@ exports.vehicles = functions.https.onRequest(async (req, res) => {
     res.json(vehicles);
 });
 
-/*
-    FETCH SPECIFIC VEHICLE ACCORDING TO UNIQUE ID
-*/
+// FETCH SPECIFIC VEHICLE ACCORDING TO UNIQUE ID
 exports.vehicle = functions.https.onRequest(async (req, res) => {
     const snapshot = await admin.firestore().collection("vehicles").doc(req.body.id).get();
 
     res.json(snapshot.data());
 });
+
+exports.addUser = functions.https.onRequest(async (req, res) => {
+    const userId = req.body.uid;
+    const userEmail = req.body.email;
+
+    const snapshot = await admin.firestore().collection("users").doc(userId).get();
+
+    if(!snapshot.exists) {
+        await admin.firestore().collection("users").doc(userId).set({
+            uid: userId,
+            email: userEmail,
+            displayName: '',
+            favorites: []
+        });
+        res.json({success: true});
+    }
+    else {
+        res.json(snapshot.data());
+    }
+})
